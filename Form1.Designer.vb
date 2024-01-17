cf6d6089684c25667b4c031f149b1b49ec61ed1d
Academy-User <Academy-User@DESKTOP-1L3B0TP>
2024-01-17 11:00:04 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.PeachPuff
+        Button1.Location = New Point(63, 153)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "Area is"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.PeachPuff
+        Button2.Location = New Point(63, 208)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(112, 34)
+        Button2.TabIndex = 0
+        Button2.Text = "Perimeter is"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(63, 49)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(111, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Enter Length"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(63, 98)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(110, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "Enter Height"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(249, 54)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(249, 95)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 31)
+        TextBox2.TabIndex = 2
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(249, 153)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(150, 31)
+        TextBox3.TabIndex = 2
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(249, 208)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(150, 31)
+        TextBox4.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.SeaShell
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim A As Double
+        Dim H As Double
+        Dim L As Double
+        Dim P As Double
+        L = Val(TextBox1.Text)
+        H = Val(TextBox2.Text)
+
+        A = L * H
+        P = 2 * (L + H)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim A As Double
+        Dim H As Double
+        Dim L As Double
+        Dim P As Double
+        L = Val(TextBox1.Text)
+        H = Val(TextBox2.Text)
+
+        A = L * H
+        P = 2 * (L + H)
+    End Sub
+End Class
